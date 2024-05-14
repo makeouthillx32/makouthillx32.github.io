c214209797a891cdbde312193061156830fad3b5
unenter <63877528+makeouthillx32@users.noreply.github.com>
2024-05-13 19:05:36 -0700

    hero 2 background update

--- js/hero2.js
+++ js/hero2.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const hero2 = document.querySelector('.hero2');
     if (hero2) {
+        console.log('Hero2 element found:', hero2);
+
         const modelViewerContainer = document.createElement('div');
         modelViewerContainer.className = 'model-viewer-container';
+        console.log('Model Viewer Container created:', modelViewerContainer);
 
         const modelViewer = document.createElement('model-viewer');
         modelViewer.src = 'images/unenterlogom.glb'; // Update the path if necessary
@@ -10,8 +13,12 @@ document.addEventListener('DOMContentLoaded', function() {
         modelViewer.setAttribute('auto-rotate', '');
         modelViewer.setAttribute('camera-controls', '');
         modelViewer.classList.add('model-viewer'); // Use for specific styling
+        console.log('Model Viewer created:', modelViewer);
 
         modelViewerContainer.appendChild(modelViewer);
         hero2.appendChild(modelViewerContainer);
+        console.log('Elements appended to Hero2:', hero2);
+    } else {
+        console.error('Hero2 element not found');
     }
 });
